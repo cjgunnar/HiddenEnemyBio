7cb4bd274c67b4c3b3459f78deb66f4df49bd41d
cjgunnar <34259593+cjgunnar@users.noreply.github.com>
2023-09-04 10:31:26 -0500

    fix unhidden bio in inspector tab

---
+++ Source/HiddenEnemyBio/Patch_CharacterCardUtility_DrawCharacterCard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Verse;
+using HarmonyLib;
+using Verse.Sound;
+
+namespace HiddenEnemyBio
+{
+    [HarmonyPatch(typeof(CharacterCardUtility), "DrawCharacterCard")]
+    public static class Patch_CharacterCardUtility_DrawCharacterCard
+    {
+        static bool Prefix(Rect rect, Pawn pawn, Action randomizeCallback, Rect creationRect, bool showName)
+        {
+            if(HiddenBioUtil.ShouldBioVisible(pawn))
+            {
+                return true;
+            }
+            else
+            {
+                CharacterIntelUtility.DrawCharacterCard(rect, pawn, randomizeCallback, creationRect, showName);
+                return false;
+            }
+        }
+    }
+}
